b2e7a0fd76856a6764772e2ba1b593060e4bec43
João Gabriel <joao951biel@gmail.com>
2022-08-25 20:39:31 -0300

    passo 1 do projeto

--- projeto-08.02/js/script.js
+++ projeto-08.02/js/script.js
@@ -1,19 +1,20 @@
-let xhttp = new XMLHttpRequest();
-let lsProduto = [];
+let xhttp = new XMLHttpRequest()
+let lsProduto = []
 
 function buscarProduto() {
   xhttp.open('GET', 'https://pascoa-chiquinha.herokuapp.com/produto/')
   xhttp.send()
   xhttp.onload = function () {
-    lsProduto = this.response;
-    lsProduto = JSON.parse(lsProduto);
-    mostraListaProdutosHtml(lsProduto);
+    lsProduto = this.response
+    lsProduto = JSON.parse(lsProduto)
+    mostraListaProdutosHtml(lsProduto)
+    marcarPodrutosSelecionadosLocalStorage()
   }
 }
 
 function mostraListaProdutosHtml(lsProduto) {
-  let listaProduto = "";
-  let i = 0;
+  let listaProduto = ''
+  let i = 0
   for (produto of lsProduto) {
     listaProduto += `
     <div class="embrulho">
@@ -24,31 +25,52 @@ function mostraListaProdutosHtml(lsProduto) {
       </p>
       <i class="material-icons carrinho" onclick="addProdutoCarrinho(${i})" >&#xe8cc;</i>
     </div>
-  </div> `;
-   produto.carrinho = false;
-   i++;
+  </div> `
+    produto.carrinho = false
+    i++
   }
-  document.getElementById("listaProduto").innerHTML = listaProduto;
+  document.getElementById('listaProduto').innerHTML = listaProduto
 }
 
-function addProdutoCarrinho(i) {
-  let produto = lsProduto[i];
-  if(produto.carrinho == false) {
-    produto.carrinho = true;
-    document.getElementsByClassName("material-icons carrinho")[i].style.color = "#e66b6b";
-  }else{
-    produto.carrinho = false;
-    document.getElementsByClassName("material-icons carrinho")[i].style.color = "#0000007d";
-  }
-
-}
-
-function verListaProdutosSelecionados(){
-  for (produto of lsProduto) {
-    if(produto.carrinho){
-      
+function marcarPodrutosSelecionadosLocalStorage() {
+  let lista = localStorage.getItem('listaProdutoLocalStorage')
+  lista = JSON.parse(lista)
+  for (i in lista) {
+    if (lista[i].carrinho) {
+      addProdutoCarrinho(i)
     }
   }
 }
 
-buscarProduto();
+function addProdutoCarrinho(i) {
+  let produto = lsProduto[i]
+  if (produto.carrinho == false) {
+    produto.carrinho = true
+    document.getElementsByClassName('material-icons carrinho')[i].style.color =
+      '#e66b6b'
+  } else {
+    produto.carrinho = false
+    document.getElementsByClassName('material-icons carrinho')[i].style.color =
+      '#0000007d'
+  }
+  localStorage.setItem('listaProdutoLocalStorage', JSON.stringify(lsProduto))
+}
+
+function verListaProdutosSelecionados() {
+  for (produto of lsProduto) {
+    if (produto.carrinho) {
+      listaProduto += `
+    <div class="embrulho">
+    <div class="produtos">
+      <img src="${produto.imagem}" alt="" />
+      <p>${produto.nome}
+        <span class="valor">${produto.valor.toFixed(2)}</span>
+      </p>
+      <i class="material-icons carrinho" onclick="addProdutoCarrinho(${i})" >&#xe8cc;</i>
+    </div>
+  </div> `;
+    }
+  }
+}
+
+buscarProduto()
